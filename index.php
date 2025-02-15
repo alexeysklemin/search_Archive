f2cd57d6cf3691f762821d3021c637cfa9f532fd
alexeysklemin <a76sklemin@gmail.com>
2025-02-15 08:10:07 +0400

    search form

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', 1);
 ini_set('diplay_startup_errors', 1);
 error_reporting(E_ALL);
@@ -7,7 +8,7 @@ error_reporting(E_ALL);
 $DB_HOST = 'localhost';
 $DB_USER = 'www-data'; 
 $DB_PASSWORD = 'some_password';
-$DB_NAME = 'test';
+$DB_NAME = 'archive';
 
 $connection = new mysqli ($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
@@ -15,19 +16,24 @@ if($connection->connect_error){
 	die("Error connection" . $connection->connect_error);
 }else{
 
-$sql = "SELECT * FROM `test`";
+$sql = "SELECT * FROM `area`";
 
 $result = $connection->query($sql);
 
 while($row = $result->fetch_assoc()){
 
 echo  nl2br($row['name']);
-echo   "<a href = '".$row['site']."'>  Homepage  </a>";
+echo   "<a href = '".$row['ste_address']."'>  Homepage  </a>";
 echo	'<br>';
 }
 
 }
 $connection->close();
+
 ?>
 
+<form action="" method="get">
+    <input type="text" name="query" placeholder="Введите запрос...">
+    <button type="submit">Искать</button>
+</form>
 
